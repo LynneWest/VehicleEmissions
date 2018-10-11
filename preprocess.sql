ddd16a3568d036416abd3e6b26ff7b7ff8c8f262
Lynne West <thislynnewest@gmail.com>
2018-10-11 10:44:09 -0700

    Combine redundant VClass values

---
+++ preprocess.sql
@@ -0,0 +1,23 @@
+UPDATE vehicle
+SET VClass = 'Special Purpose Vehicle 2WD'
+WHERE VClass = 'Special Purpose Vehicles/2wd';
+
+UPDATE vehicle
+SET VClass = 'Special Purpose Vehicle 4WD'
+WHERE VClass = 'Special Purpose Vehicles/4wd';
+
+UPDATE vehicle
+SET VClass = 'Standard Pickup Trucks 2WD'
+WHERE VClass = 'Standard Pickup Trucks/2wd'
+
+UPDATE vehicle
+SET VClass = 'Vans, Passenger Type'
+WHERE VClass = 'Vans Passenger'
+
+UPDATE vehicle
+SET VClass = 'Sport Utility Vehicle - 4WD'
+WHERE VClass = 'Standard Sport Utility Vehicle 4WD'
+
+UPDATE vehicle
+SET VClass = 'Sport Utility Vehicle - 2WD'
+WHERE VClass = 'Standard Sport Utility Vehicle 2WD'
