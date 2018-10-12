e731778225502181a5df9014599b28fdb5d6cb75
Lynne West <thislynnewest@gmail.com>
2018-10-11 17:06:28 -0700

    Combine redundant VClass values

--- preprocess_VClass.sql
+++ preprocess_VClass.sql
@@ -1,24 +1,23 @@
 --Combine redundant VClass values
 UPDATE vehicle
-SET VClass = 'Special Purpose Vehicle 2WD'
-WHERE VClass = 'Special Purpose Vehicles/2wd';
+SET VClass = 'Small Pickup Truck'
+WHERE VClass = 'Small Pickup Trucks 4WD' OR VClass = 'Small Pickup Trucks 2WD' OR VClass = 'Small Pickup Trucks' OR VClass = 'Small Truck';
 
 UPDATE vehicle
-SET VClass = 'Special Purpose Vehicle 4WD'
-WHERE VClass = 'Special Purpose Vehicles/4wd';
+SET VClass = 'Standard Pickup Truck'
+WHERE VClass = 'Standard Pickup Trucks/2wd' OR VClass = 'Standard Pickup Trucks 2WD' OR VClass = 'Standard Pickup Trucks 4WD' OR VClass = 'Standard Pickup Trucks 2WD' or VClass = 'Standard Pickup Trucks'
 
 UPDATE vehicle
-SET VClass = 'Standard Pickup Trucks 2WD'
-WHERE VClass = 'Standard Pickup Trucks/2wd'
+SET VClass = 'Sport Utility Vehicle'
+WHERE VClass = 'Small Sport Utility Vehicle 2WD' OR VClass = 'Small Sport Utility Vehicle 4WD' OR VClass = 'Sport Utility Vehicle - 2WD' OR VClass = 'Sport Utility Vehicle - 4WD' OR VClass = 'Standard Sport Utility Vehicle 4WD' OR VClass = 'Standard Sport Utility Vehicle 2WD';
 
 UPDATE vehicle
-SET VClass = 'Vans, Passenger Type'
-WHERE VClass = 'Vans Passenger'
+SET VClass = 'Station Wagon'
+WHERE VClass = 'Midsize Station Wagons' OR VClass = 'Midsize-Large Station Wagons' OR VClass = 'Small Station Wagons';
 
 UPDATE vehicle
-SET VClass = 'Sport Utility Vehicle - 4WD'
-WHERE VClass = 'Standard Sport Utility Vehicle 4WD'
+SET VClass = 'Van'
+WHERE VClass = 'Vans, Passenger Type' OR VClass = 'Vans, Cargo Type' OR VClass = 'Minivan' OR VClass = 'Vans Passenger' OR VClass = 'Minivan - 4WD' OR VClass = 'Minivan - 2WD' OR VClass = 'Vans'
 
-UPDATE vehicle
-SET VClass = 'Sport Utility Vehicle - 2WD'
-WHERE VClass = 'Standard Sport Utility Vehicle 2WD'
+DELETE FROM vehicle
+WHERE VClass = 'Special Purpose Vehicle' OR VClass = 'Special Purpose Vehicles/2wd' OR VClass = 'Special Purpose Vehicles/4wd' OR VClass = 'Special Purpose Vehicle 4WD' OR VClass = 'Special Purpose Vehicle 2WD' OR VClass = 'Special Purpose Vehicles';
