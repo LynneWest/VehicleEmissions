cb7856f891d3207bd83da1750f28fcf5e5b2ad12
Lynne West <thislynnewest@gmail.com>
2018-10-13 12:54:37 -0700

    remove query

--- DataA.sql
+++
@@ -1,8 +0,0 @@
---Select data to answer vehicle emission question A
-SELECT VClass, co2TailpipeGpm
-FROM VehicleEmissionsA
-WHERE fuelType2 IS NULL
-AND atvType IS NULL
-AND fuelType1 != 'Natural Gas'
-AND fuelType1 != 'Electricity'
-AND fuelType1 != 'Diesel'
