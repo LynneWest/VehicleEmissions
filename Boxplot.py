3bc249ffe3ed26632e6d965a622eecd45534abb5
Lynne West <thislynnewest@gmail.com>
2018-10-12 19:24:30 -0700

    Create boxplot to answer question A

--- Boxplot.py
+++ Boxplot.py
@@ -14,6 +14,8 @@ cur = cnxn.cursor()
 cur.execute("SELECT co2TailpipeGpm FROM vehicle WHERE VClass = 'Compact Cars'")
 data = np.array(cur.fetchall())
 
-
-plt.boxplot(data, showfliers=False, vert=False)
+fig, ax1 = plt.subplots()
+label = ['Compact Cars']
+plt.boxplot(data, showfliers=False)
+ax1.set_xticklabels(label)
 plt.show()
