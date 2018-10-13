4dfe4910a126ee30cbabdbbf0cb614c68192e7a7
Lynne West <thislynnewest@gmail.com>
2018-10-13 13:17:23 -0700

    Combine redundant VClass values

--- preprocess_VClass.sql
+++ preprocess_VClass.sql
@@ -1,11 +1,8 @@
 --Combine redundant VClass values
 UPDATE vehicle
-SET VClass = 'Small Pickup Truck'
-WHERE VClass = 'Small Pickup Trucks 4WD' OR VClass = 'Small Pickup Trucks 2WD' OR VClass = 'Small Pickup Trucks' OR VClass = 'Small Truck';
-
-UPDATE vehicle
-SET VClass = 'Standard Pickup Truck'
-WHERE VClass = 'Standard Pickup Trucks/2wd' OR VClass = 'Standard Pickup Trucks 2WD' OR VClass = 'Standard Pickup Trucks 4WD' OR VClass = 'Standard Pickup Trucks 2WD' or VClass = 'Standard Pickup Trucks'
+SET VClass = 'Truck'
+WHERE VClass = 'Small Pickup Trucks 4WD' OR VClass = 'Small Pickup Trucks 2WD' OR VClass = 'Small Pickup Trucks' OR VClass = 'Small Truck' OR VClass = 'Standard Pickup Trucks/2wd' OR VClass = 'Standard Pickup Trucks 2WD' OR VClass = 'Standard Pickup Trucks 4WD' OR VClass = 'Standard Pickup Trucks 2WD' or VClass = 'Standard Pickup Trucks'
+;
 
 UPDATE vehicle
 SET VClass = 'Sport Utility Vehicle'
@@ -17,7 +14,12 @@ WHERE VClass = 'Midsize Station Wagons' OR VClass = 'Midsize-Large Station Wagon
 
 UPDATE vehicle
 SET VClass = 'Van'
-WHERE VClass = 'Vans, Passenger Type' OR VClass = 'Vans, Cargo Type' OR VClass = 'Minivan' OR VClass = 'Vans Passenger' OR VClass = 'Minivan - 4WD' OR VClass = 'Minivan - 2WD' OR VClass = 'Vans'
+WHERE VClass = 'Vans, Passenger Type' OR VClass = 'Vans, Cargo Type' OR VClass = 'Minivan' OR VClass = 'Vans Passenger' OR VClass = 'Minivan - 4WD' OR VClass = 'Minivan - 2WD' OR VClass = 'Vans';
 
-DELETE FROM vehicle
-WHERE VClass = 'Special Purpose Vehicle' OR VClass = 'Special Purpose Vehicles/2wd' OR VClass = 'Special Purpose Vehicles/4wd' OR VClass = 'Special Purpose Vehicle 4WD' OR VClass = 'Special Purpose Vehicle 2WD' OR VClass = 'Special Purpose Vehicles';
+UPDATE vehicle
+SET VClass = 'Car'
+WHERE VClass = 'Midsize Cars' OR VClass = 'Large Cars';
+
+UPDATE vehicle 
+SET VClass = 'Compact Car'
+WHERE VClass = 'Minicompact Cars' OR VClass = 'Compact Cars' OR VClass = 'Subcompact Cars';
