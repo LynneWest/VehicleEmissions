94b2ffe57222651189f0cdd75bceacfb1a62eb84
Lynne West <thislynnewest@gmail.com>
2018-10-13 15:19:33 -0700

    Create boxplots to compare CO2 emissions by vehicle class

--- Boxplot.py
+++ Boxplot.py
@@ -1,4 +1,4 @@
-#Create boxplots to answer question A
+#Create boxplots to compare CO2 emissions by vehicle class
 import pyodbc
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,23 +29,25 @@ cc = np.array(cc.fetchall())
 suv = cur.execute(query_string + "'Sport Utility Vehicle';")
 suv = np.array(suv.fetchall())
 
-sw = cur.execute(query_string + "'Station Wagon';")
-sw = np.array(sw.fetchall())
-
 truck = cur.execute(query_string + "'Truck';")
 truck = np.array(truck.fetchall())
 
 van = cur.execute(query_string + "'Van';")
 van = np.array(van.fetchall())
 
+data = [car, cc, suv, truck, van]
 
-
-
-
-data = [car, cc, suv, sw, truck, van]
-
-fig, ax1 = plt.subplots()
-label = ['Cars', 'Compact Cars', 'Sport Utility Vehicles', 'Station Wagons', 'Trucks', 'Vans']
-plt.boxplot(data, vert=False)
-ax1.set_yticklabels(label)
+fig, ax = plt.subplots()
+label = ['Cars', 'Compact Cars', 'Sport Utility Vehicles', 'Trucks', 'Vans']
+box = plt.boxplot(data, vert=False, patch_artist=True)
+plt.setp(box['boxes'], facecolor='lightblue')
+plt.setp(box['fliers'], markersize=3)
+plt.setp(box['medians'], color='darkblue')
+ax.set_xlim(100,1200)
+plt.xticks([200,300,400,500,600,700,800,900,1000,1100])
+ax.xaxis.grid(True, color='lightgrey')
+ax.set_axisbelow(True)
+ax.set_yticklabels(label)
+ax.set_xlabel('Tailpipe CO2 in grams/mile')
+ax.set_title('CO2 emissions by vehicle class')
 plt.show()
